425d8771402f621a8eab3f83abebc25d7095043d
Benjamin San Souci <benjamin.sansouci@gmail.com>
2016-10-25 19:22:46 -0700

    Allow .config.js for webpack.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/jsFile1.js',
+  output: {
+    filename: './_build/all.js',
+  },
+  module: {
+    loaders: [
+      {
+        include: [
+          path.resolve(__dirname, src),
+        ],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
