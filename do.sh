f3aafdb022c3ffeace4e6f8114199986c80ec325
Sean Grove <sean@bushi.do>
2016-10-30 14:59:38 +0100

    Move project structure around to be a bit more straight-forward

--- do.sh
+++ do.sh
@@ -8,15 +8,11 @@ set -e
 
 ./node_modules/ocaml/bin/ocamldep -ppx ./node_modules/reason/reactjs_jsx_ppx.native -ppx ./node_modules/bs-platform/bin/bsppx.exe -pp ./node_modules/reason/refmt_impl.native -sort -ml-synonym .re src/**/*.re \
 | sed "s/src\//-impl src\//g" \
-| xargs ./node_modules/bs-platform/bin/bsc.exe -ppx ./node_modules/reason/reactjs_jsx_ppx.native -bin-annot -pp ./node_modules/reason/refmt_impl.native -I src/ -I src/nested/
+| xargs ./node_modules/bs-platform/bin/bsc.exe -ppx ./node_modules/reason/reactjs_jsx_ppx.native -bin-annot -pp ./node_modules/reason/refmt_impl.native -I src/ -I src/utils/
 
-foo=$(./node_modules/.bin/babel --plugins transform-react-createelement-to-jsx ./src/reFile.js \
+foo=$(./node_modules/.bin/babel --plugins transform-react-createelement-to-jsx ./src/index.js \
 | ./node_modules/.bin/esformatter --plugins esformatter-jsx --config eslintConfig.json)
-echo "$foo" > ./src/reFile.js
-
-foo=$(./node_modules/.bin/babel --plugins transform-react-createelement-to-jsx ./src/nested/reFile2.js \
-| ./node_modules/.bin/esformatter --plugins esformatter-jsx --config eslintConfig.json)
-echo "$foo" > ./src/nested/reFile2.js
+echo "$foo" > ./src/index.js
 
 # ./node_modules/.bin/jscodeshift -t ./node_modules/react-codemod/transforms/class.js src/**/*.js
 
